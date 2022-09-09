a9180e12b2aceb722be085929547793db9742f26
Hirthick Raj DK <95635270+hirthickraj2015@users.noreply.github.com>
2022-09-09 19:32:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@
     document.querySelectorAll("img")[0].setAttribute("src", imgend);
 
     if(dice>img1){
-        document.querySelector("h1").innerHTML="💥 Player 1 won";
+        document.querySelector("h1").innerHTML="💥 Player 2 won";
     }
     else if(img1>dice){
-        document.querySelector("h1").innerHTML="Player 2 won 💥";
+        document.querySelector("h1").innerHTML="Player 1 won 💥";
     }
     else{
         document.querySelector("h1").innerHTML="💥 Its a draw 💥";
